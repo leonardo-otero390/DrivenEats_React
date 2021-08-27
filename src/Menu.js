53429804e12511cb2fe8a50a3355af2a060900aa
Leonardo <leonardo.otero390@gmail.com>
2021-08-27 13:47:19 -0400

    Select items

--- src/Menu.js
+++ src/Menu.js
@@ -1,24 +1,36 @@
+import React, { useState } from "react";
+
 function MenuOption(props) {
     const { name, img, description, price } = props.menu;
     const { src, alt } = img;
+    const [selection, setSelection] = useState("");
     return (
-        <li className="option">
+        <li className={"option " + selection} onClick={() => setSelection("selected")}>
             <img src={src} alt={alt} />
             <h4 className="option-name"> <strong> {name}</strong></h4>
             <p className="option-description"> {description}</p>
-            <h4 className="option-price"><strong> {price}</strong>
-                <ion-icon name="checkmark-circle-sharp"></ion-icon>
-            </h4>
+            <footer>
+                <h4 className="option-price"><strong> {price}</strong></h4>
+                <div className="counter">
+                    <button className="decrement">-</button>
+                    <h4><strong>1</strong></h4>
+                    <button className="increment">+</button>
+                </div>
+            </footer>
         </li>
     );
 }
 export default function Menu(props) {
-    const {name,type} = props.menu;
+    const { name, type } = props.menu;
+    let menuTitle;
+    if (type === 'meal') menuTitle = "Primeiro seu prato"; else if (type === 'drink') {
+        menuTitle = "Agora a bebida";
+    } else menuTitle = "Por fim um docinho";
     return (
         <section className={"menu " + type}>
-            <h2 className="menu-name">Primeiro seu prato</h2>
+            <h2 className="menu-name">{menuTitle}</h2>
             <ul className="options">
-                {name.map((option,index) =><MenuOption menu={option} key={index}/>)}
+                {name.map((option, index) => <MenuOption menu={option} key={index} />)}
             </ul>
         </section>
     );
