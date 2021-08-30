028d0eb4c7e4366091bee4998bd1642974a6673c
Leonardo <leonardo.otero390@gmail.com>
2021-08-30 16:05:00 -0400

    Router started to confirm screen

--- src/Container.js
+++ src/Container.js
@@ -1,12 +1,7 @@
 import Menu from "./Menu";
+import SendButton from "./SendButton";
 import { menuList } from "./MenuData";
 import React, { useState } from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
 
 
 export default function Container() {
@@ -103,87 +98,5 @@ export default function Container() {
             {menuList.map((menu, index) => <Menu menu={menu} key={index} trash={updateTrash} />)}
             <SendButton sendStatus={sendStatus} totalOrder={totalOrder} />
         </main>
-    );
-}
-function SendButton({ sendStatus, totalOrder }) {
-    const phoneNumber = 5567992727452;
-    const wppURLBasic = `https://wa.me/${phoneNumber}?text=`;
-    const { mealOrder, drinkOrder, dessertOrder } = totalOrder;
-
-    function orderText(order) {
-        if (order.quantity > 1) {
-            return `${order.name} (${order.quantity}X)`;
-        }
-        return `${order.name}`;
-    }
-    function orderTotalPrice() {
-        let totalPrice = 0;
-        let thisPriceInNumber;
-        for (let i = 0; i < mealOrder.length; i++) {
-            thisPriceInNumber = Number(mealOrder[i].price.replace('R$', '').replace(',', '.'));
-            totalPrice += thisPriceInNumber * mealOrder[i].quantity;
-        }
-        for (let i = 0; i < drinkOrder.length; i++) {
-            thisPriceInNumber = Number(drinkOrder[i].price.replace('R$', '').replace(',', '.'));
-            totalPrice += thisPriceInNumber * drinkOrder[i].quantity;
-        }
-        for (let i = 0; i < dessertOrder.length; i++) {
-            thisPriceInNumber = Number(dessertOrder[i].price.replace('R$', '').replace(',', '.'));
-            totalPrice += thisPriceInNumber * dessertOrder[i].quantity;
-        }
-        return `R$ ${totalPrice.toFixed(2)}`;
-    }
-    const message = `Olá, gostaria de fazer o pedido:
-    - Prato: ${mealOrder.map((m) => orderText(m))}
-    - Bebida: ${drinkOrder.map((m) => orderText(m))}
-    - Sobremesa: ${dessertOrder.map((m) => orderText(m))}
-    -Total: ${orderTotalPrice()}`;
-    function sendMessage() {
-
-        if (sendStatus === "send-ready") {
-            const URI = wppURLBasic + encodeURIComponent(message);
-            window.open(URI)
-        }
-    }
-    function Review() {
-        return (
-            <div className={sendStatus?"confirm-screen":"hidden"}>
-                <div className="confirm-background"></div>
-                <div className="confirm-square">
-                    <h2 className="confirm-title"><strong>confirme seu pedido</strong></h2>
-                    <div className="review">
-                        oeied2e
-                    </div>
-                    <button onClick={sendMessage}> <strong> Tudo certo, pode pedir! </strong></button>
-                    <Link to="/">
-                    <h2>Cancelar</h2>
-                    </Link>
-                </div>
-            </div>
-        )
-    }
-
-    return (
-
-        <Router>
-            <Route>
-                <Link to="/revisao" onClick={e => sendStatus?e :e.preventDefault()}>
-                    <div className="button-bar">
-                        <button className={"send-request " + sendStatus}>
-                            {sendStatus ? "Fechar pedido" : <h1>Selecione os 3 itens <br /> para fechar o pedido</h1>}
-                        </button>
-                    </div>
-                </Link>
-            </Route>
-            <Switch>
-                <Route path="/revisao">
-                    <Review />
-                </Route>
-            </Switch>
-        </Router>
-
-
-
-
     );
 }
